f1aa69251aa5186be85d9d281a21ee5ec36ed6a1
Emerfoll <66702595+Emerfoll@users.noreply.github.com>
2021-04-27 20:51:42 -0500

    Portfolio started...

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <h1>Chris Emerfoll</h1>
+        <h2>Full-Stack Web Developer</h2>
+      </header>
+
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
